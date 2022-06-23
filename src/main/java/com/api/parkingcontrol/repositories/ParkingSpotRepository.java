ca83025de44d9bd0785e1f0892c00a0076fdc023
Gabriel Yuto <gabrielyuto2016@gmail.com>
2022-06-23 14:44:13 -0300

    first commit

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String licensePlateCar);
+    boolean existsByApartmentAndBlock(String licensePlateCar, String block);
+}
